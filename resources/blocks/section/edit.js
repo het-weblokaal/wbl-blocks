8025aa9705f4464033d5fccd49532e50d745932d
Erik Joling <erik@ejoweb.nl>
2020-09-11 14:46:40 +0200

    First try at section block

---
+++ resources/blocks/section/edit.js
@@ -0,0 +1,34 @@
+/**
+ * Internal dependencies
+ */
+
+/**
+ * WordPress dependencies
+ */
+const { RichText } = wp.blockEditor;
+const { __ } = wp.i18n;
+
+/**
+ * Edit function
+ */
+export default function edit( { attributes, setAttributes, className } ) {
+
+	const { content } = attributes;
+
+    return (
+        <div className={ className }>
+        	<div className={ `${className}__inner` }>
+            	<RichText
+					className={ `${className}__content` }
+					tagName="div"
+                	multiline="p"
+					value={ content }
+					onChange={ ( content ) => { setAttributes( { content } ) } }
+					placeholder={ __( 'Test', 'hwl-blokkendoos' ) }
+					keepPlaceholderOnFocus={true}
+				/>
+            </div>
+        </div>
+    );
+};
+
---
+++ resources/blocks/section/save.js
@@ -0,0 +1,29 @@
+/**
+ * Internal dependencies
+ */
+
+/**
+ * WordPress dependencies
+ */
+const { RichText } = wp.blockEditor;
+const { getBlockDefaultClassName } = wp.blocks;
+
+/**
+ * Save function
+ */
+export default function save( { attributes } ) {
+
+	// Extract variables
+	const { content } = attributes;
+
+	// Get className (https://github.com/WordPress/gutenberg/issues/7308#issuecomment-439946166)
+	const className = getBlockDefaultClassName( 'hwl-blokkendoos/test' );
+
+	return (
+		<div className={ className }>
+			<div className={ `${className}__inner` }>
+				<RichText.Content className={ `${className}__content` } tagName="div" value={ content } />
+			</div>
+		</div>
+	);
+};
